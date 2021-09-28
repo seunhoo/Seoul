3887d01d486649c9efff734746c14e210b82776c
seunhoo <seunhoo@naver.com>
2021-09-28 18:18:05 +0900

    격투게임 시작

---
+++ Space/MainScene.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "MainScene.h"
+
+
+MainScene::MainScene()
+	:m_Check(false)
+{
+}
+
+MainScene::~MainScene()
+{
+}
+
+void MainScene::Init()
+{
+
+}
+
+void MainScene::Release()
+{
+}
+
+void MainScene::Update(float deltaTime, float time)
+{
+
+}
+
+void MainScene::Render()
+{
+}
